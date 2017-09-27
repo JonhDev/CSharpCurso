aa76415b819cabcc25a26247428533894b99cbc6
Jonathan Bravo <jonhdev01@outlook.com>
2017-09-27 13:04:55 -0500

    Agregar archivos de proyecto.

---
+++ ClaseMetodos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
